3eb6e0ecd4d4571c7fc41427351e141debd2d730
Rodney van den Velden <rodneymichael2002@gmail.com>
2025-04-05 12:42:18 +0200

    forgot exports

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
 export * from './classes/commandclient.ts';
+export * from './classes/commandcontext.ts';
+export * from './classes/commandsloader.ts';
+export * from './classes/commandsmanager.ts';
+export * from './classes/command.ts';
